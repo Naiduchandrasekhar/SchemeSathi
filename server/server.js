9a0cac2afce7c81225b9497bebd9c61e9afff506
Naiduchandrasekhar <naiduchandrasekhar10@gmail.com>
2026-07-19 18:56:06 +0530

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -16,16 +16,10 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin(origin, callback) {
-      // Allow Postman, curl, server-to-server requests
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error(`Origin ${origin} not allowed by CORS`));
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://tubular-wisp-c99977.netlify.app",
+    ],
     credentials: true,
   })
 );
